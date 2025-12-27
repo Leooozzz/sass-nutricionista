151184b042dde0134753179bb5c6a8e41e7a1c09
Leooozzz <leozeraffcanal2405@gmail.com>
2025-12-27 12:02:38 -0300

    finish register form

--- src/app/(site)/register/page.tsx
+++ src/app/(site)/register/page.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 
 export default function Page () {
     return(
-        <div className="max-w-md mx-auto">
-            <Image src={"/images/image_LogoSingIn_Up.png"} alt={""} height={400} width={400}/>
+        <div className="max-w-xl mx-auto bg-white dark:bg-black p-5 rounded-md">
+            <div className="flex justify-center items-center">
+                <Image src={"/images/image_LogoSingIn_Up.png"} alt={""} height={400} width={400} />
+            </div>
                 <RegisterForm/>
         </div>
     )
--- src/auth/registerForm.tsx
+++ src/auth/registerForm.tsx
@@ -4,6 +4,9 @@ import { ChangeEvent, FormEvent, useState, useTransition } from "react"
 import { ErrorStructure, schema } from "./schema/registerSchema"
 import { register } from "@/actions/register"
 import { redirect } from "next/navigation"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 
 export const RegisterForm = () =>{
@@ -31,8 +34,8 @@ export const RegisterForm = () =>{
         }setError({})
         startTransition(async()=>{
             const res = await register(form)
-            if(result.error){
-                setError({form  : res.error})
+            if(res.error){
+                setError({form : res.error})
             }else{
                 redirect('/login')
             }
@@ -41,8 +44,32 @@ export const RegisterForm = () =>{
 
 
     return(
-        <form action="" onSubmit={handleSubmit}>
+        <form action="" onSubmit={handleSubmit} className="p-8 border border-gray-100 rounded-sm ">
+            <h2 className="text-2xl font-bold flex text-green-700 justify-center mb-5">Cadastrar</h2>
+            <div className="mb-4">
+                <Input type="text" name="name" value={form.name} onChange={handleChange} autoFocus className="w-full p-5" disabled={pending} placeholder="Digite seu usuario"/>
+                {error.name && <div className='text-red-500 text-sm mt-1'>{error.name}</div>}
+            </div>
 
+             <div className="mb-4">
+                <Input type="email" name="email" value={form.email} onChange={handleChange} autoFocus className="w-full p-5" disabled={pending} placeholder="email"/>
+                {error.email && <div className='text-red-500 text-sm mt-1'>{error.email}</div>}
+            </div>
+
+             <div className="mb-4">
+                <Input type="password" name="password" value={form.password} onChange={handleChange} autoFocus className="w-full p-5" disabled={pending} placeholder="Senha"/>
+                {error.password && <div className='text-red-500 text-sm mt-1'>{error.password}</div>}
+            </div>
+
+             <div className="mb-4">
+                <Input type="password" name="confirmPassword" value={form.confirmPassword} onChange={handleChange} autoFocus className="w-full p-5" disabled={pending} placeholder="Confirme sua senha"/>
+                {error.confirmPassword && <div className='text-red-500 text-sm mt-1'>{error.confirmPassword}</div>}
+            </div>
+            <Button type='submit' className='w-full bg-green-700 text-white hover:bg-green-300 p-5' disabled={pending}>
+            {pending ? 'Cadastrar...' : 'Cadastrando'}
+            </Button>
+            {error.form && <div className='text-red-500 text-sm mt-1'>{error.form}</div>}
+            <Link href={'/login'} className='flex justify-center text-green-700 text-sm mt-5'>Ja tem conta? faça login!</Link>
 
         </form>
     )
